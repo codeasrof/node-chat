1e241c7a1aee4e4abdd83fd8bcf7be15cda6e7ed
codeasrof <wid_rrodrigues@uolinc.com>
2023-09-11 14:32:50 -0300

    adding authPage

--- frontend/src/AuthPage.jsx
+++ frontend/src/AuthPage.jsx
@@ -1,6 +1,31 @@
-const AuthPage = () => {
+const AuthPage = (props) => {
+  const onSubmit = (e) => {
+    e.preventDefault()
+    const {value} = e.target[0]
+    // eslint-disable-next-line react/prop-types
+    props.onAuth({username: value, secret:value})
+  }
   return (
-    <div>AuthPage</div>
+    <div className="background">
+        <form onSubmit={onSubmit} className="form-card">
+            <div className="form-title">
+                Welcome 
+            </div> 
+            <div className="form-subtitle">
+                Set a username to get started
+            </div>
+
+            <div className="auth">
+                <div className="auth-label">
+                    Username
+                </div>
+                <input className="auth-input" name="username" />
+                <button className="auth-button" type="submit">
+                        Enter
+                </button>
+            </div>
+        </form>
+    </div>
   )
 }
 
